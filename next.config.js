75a410e11461a272e5da3dd5808a86e1a4e757b8
Rana-Mapscrew <ranapaul741223@gmail.com>
2023-12-20 20:38:28 +0530

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "rana-twitter-dev.s3.ap-south-1.amazonaws.com"]
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "rana-twitter-dev.s3.ap-south-1.amazonaws.com", "http://3.6.98.64:8000/graphql"]
   }
 }
 
