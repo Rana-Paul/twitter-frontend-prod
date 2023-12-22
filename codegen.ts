a3aa42ca51cb674f5aee0e44a8647c7bf8829815
Rana-Mapscrew <ranapaul741223@gmail.com>
2023-12-22 09:18:17 +0530

    api-change

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://3.6.98.64:8000/graphql",
+  schema: "https://fullstackdeveloper.website/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
